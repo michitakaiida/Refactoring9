cd64c11fecb0ca8f5885d0b3fdd11b7a6e77d230
michiakaiida <michitaka.iida@gmail.com>
2012-04-07 22:31:51 +0900

    g

--- Refactoring_Refactored/src/条件記述の統合/CalcDisability.java
+++ Refactoring_Refactored/src/条件記述の統合/CalcDisability.java
@@ -2,7 +2,22 @@ package ğŒ‹Lq‚Ì“‡;
 
 public class CalcDisability {
 
-	public void disabiilityAmount(){
-		
+	public int disabiilityAmount() {
+
+		int seniority = 0;
+		boolean isPartTimer = false;
+		int monthDisabled = 0;
+		int amount = 10;
+
+		if (seniority < 2) {
+			amount=0;
+		}
+		if (monthDisabled > 12) {
+			amount=0;
+		}
+		if (isPartTimer) {
+			amount=0;
+		}
+		return amount;
 	}
 }
