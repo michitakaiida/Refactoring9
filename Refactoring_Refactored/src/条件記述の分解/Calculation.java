78681116c7b3bb456b5d67e0c4367f947eb66c1c
michiakaiida <michitaka.iida@gmail.com>
2012-04-07 21:45:51 +0900

    last

--- Refactoring_Refactored/src/条件記述の分解/Calculation.java
+++ Refactoring_Refactored/src/条件記述の分解/Calculation.java
@@ -5,15 +5,29 @@ public class Calculation {
 		int month = 1;
 		int charge;
 		int quantity = 5;
-		int summerRate = 10;
-		int nonSummerRate = 2;
+		// int summerRate = 10;
+		// int nonSummerRate = 2;
 
 		// TODO Auto-generated method stub
-		if (6 < month && month < 9) {
-			charge = quantity * summerRate;
+		if (isSummer(month)) {
+			charge = summerCharge(quantity);
 		} else {
-			charge = quantity * nonSummerRate;
+			charge = nonSummerCharge(quantity);
 		}
 		return charge;
+	}
+
+	private boolean isSummer(int month) {
+		return 6 < month && month < 9;
+	}
+
+	private int summerCharge(int quantity) {
+		int summerRate = 10;
+		return quantity * summerRate;
+	}
+
+	private int nonSummerCharge(int quantity) {
+		int nonSummerRate = 2;
+		return quantity * nonSummerRate;
 	}
 }
